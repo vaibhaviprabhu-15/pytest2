3d3f5a2a39c0ca5f53c66fb46c659aac15e5fea2
vaibhaviprabhu-15 <vaibhaviprabhu707@gmail.com>
2025-11-26 11:27:00 +0530

    save

---
+++ addition.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    return a+b
+if __name__== "__main__":
+    import sys
+    if len(sys.argv) != 3:
+        script_name = sys.argv[0]
+        x=int(sys.argv[1])
+        y=int(sys.argv[2])
+    else:
+        x=10
+        y=20 
+
+    print("The sum is: ", add(x, y))
